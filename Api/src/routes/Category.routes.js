c83c94cf4e007e0bffc209dada4bdc255fe0e43e
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 19:40:11 -0300

    Rotas Categoria

--- Api/src/routes/Category.routes.js
+++ Api/src/routes/Category.routes.js
@@ -2,7 +2,14 @@ const routes = require('express').Router();
 const { VerifyToken, RefreshToken } = require('../middlewares/Verify');
 
 const {
+    CreateNewCategory,
+    ListOneCategory,
+    ListCategory
     
 } = require('../controllers/Category')
+
+routes.post('/Category/Create', VerifyToken, RefreshToken, CreateNewCategory);
+routes.get('/Category/:id', VerifyToken, RefreshToken, ListOneCategory);
+routes.get('/Category', VerifyToken, RefreshToken, ListCategory);
 
 module.exports = routes;
