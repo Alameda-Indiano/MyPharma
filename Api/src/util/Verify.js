ddfccaae1babd1f3dc682aa4cac19eccb516cbf3
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 13:56:05 -0300

    Verificadores

---
+++ Api/src/util/Verify.js
@@ -0,0 +1,29 @@
+const { compareSync } = require('bcrypt');
+const { verify } = require('jsonwebtoken');
+
+module.exports = {
+    EncryptedPasswordChecker: ({ Password, PasswordToVerify }) => {
+        return compareSync(Password, PasswordToVerify);
+
+    },
+
+    TokenChecker: async (token, res, req, next) => {
+        verify(token, process.env.SECRET_JWT, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({
+                    error: true,
+                    message: err.message
+                });
+            };
+            
+            req.DataUser = { 
+                id: decoded.id, 
+                email: decoded.email
+            };
+
+            next();
+
+        });
+    },
+    
+};
