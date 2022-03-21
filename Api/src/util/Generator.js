afdf10a2bcb9a3efb5abe0eecca3af3ebb732659
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 13:55:36 -0300

    Geradores

---
+++ Api/src/util/Generator.js
@@ -0,0 +1,28 @@
+const { hashSync } = require('bcrypt');
+const { sign } = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+
+module.exports = {
+    EncryptedPasswordGenerator: (password) => {
+        return hashSync(password, 10);
+        
+    },
+
+    JWTGenerator: (params = { id, email }) => {
+        return sign(params, process.env.SECRET_JWT, {
+            expiresIn: 60 * 60 // 1h
+        });
+        
+    },
+
+    RandomCodeGenerator: () => {
+        const code = randomBytes(4).toString('hex');
+
+        const CodeExpiresIn = new Date();
+        CodeExpiresIn.setMinutes(CodeExpiresIn.getMinutes() + 10);
+
+        return { code, CodeExpiresIn };
+
+    }
+    
+};
