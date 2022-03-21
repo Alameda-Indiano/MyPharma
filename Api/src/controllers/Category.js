1278ba6d99a98343689b59c8e9c327849a052e2b
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 19:57:22 -0300

    Atualizar Categoria

--- Api/src/controllers/Category.js
+++ Api/src/controllers/Category.js
@@ -109,4 +109,54 @@ module.exports = {
 
     },
 
+    UpdateCategory: async (req, res) => {
+        const { name, description } = req.body;
+
+        try {
+            if (!name) {
+                return res.status(401).json({
+                    error: true, 
+                    message: 'É necessário informar um nome para a nova Categoria'
+                });
+            };
+
+            if (await CategoryModel.findOne({ name })) {
+                return res.status(401).json({
+                    error: true,
+                    message: 'Não é possível cadastrar duas categorias com o mesmo nome'
+                });
+            };
+
+            if (!description) {
+                return res.status(401).json({
+                    error: true, 
+                    message: 'É necessário informar uma breve descrição para a nova Categoria'
+                });
+            };
+
+            const NewCategory = await CategoryModel.findByIdAndUpdate(req.params.id, { name, description });
+
+            if (!NewCategory) {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Não foi possível cadastrar a nova categoria! Tente novamente mais tarde'
+                });
+            };
+
+            return res.status(201).json({
+                error: false,
+                category: NewCategory,
+                token: req.RefreshToken.JWT
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                error: true,
+                message: error.message
+            });
+            
+        };
+
+    }
+
 };
--- Api/src/routes/Category.routes.js
+++ Api/src/routes/Category.routes.js
@@ -4,12 +4,14 @@ const { VerifyToken, RefreshToken } = require('../middlewares/Verify');
 const {
     CreateNewCategory,
     ListOneCategory,
-    ListCategory
+    ListCategory,
+    UpdateCategory
     
 } = require('../controllers/Category')
 
 routes.post('/Category/Create', VerifyToken, RefreshToken, CreateNewCategory);
 routes.get('/Category/:id', VerifyToken, RefreshToken, ListOneCategory);
 routes.get('/Category', VerifyToken, RefreshToken, ListCategory);
+routes.put('/Category/Atualize/:id', VerifyToken, RefreshToken, UpdateCategory);
 
 module.exports = routes;
