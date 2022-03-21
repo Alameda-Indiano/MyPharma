150ff5283b6ebad4ac374edbf82895b511ce3aca
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 19:40:45 -0300

    Rotas Brand

--- Api/src/routes/Brand.routes.js
+++ Api/src/routes/Brand.routes.js
@@ -2,7 +2,14 @@ const routes = require('express').Router();
 const { VerifyToken, RefreshToken } = require('../middlewares/Verify');
 
 const {
-    
+    CreateNewBrand,
+    ListOneBrand,
+    ListBrand
+
 } = require('../controllers/Brand')
+
+routes.post('/Brand/Create', VerifyToken, RefreshToken, CreateNewBrand);
+routes.get('/Brand/:id', VerifyToken, RefreshToken, ListOneBrand);
+routes.get('/Brand', VerifyToken, RefreshToken, ListBrand);
 
 module.exports = routes;
