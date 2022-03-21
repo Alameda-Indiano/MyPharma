b4df4188664f50aa5429be7087af9e036d674afa
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 16:09:42 -0300

    Controller Criar Nova Categoria

--- Api/src/controllers/Category.js
+++ Api/src/controllers/Category.js
@@ -1,5 +1,47 @@
 const CategoryModel = require('../models/Category');
 
 module.exports = {
+    CreateNewCategory: async (req, res) => {
+        const { name, description } = req.body;
+
+        try {
+            if (!name) {
+                return res.status(401).json({
+                    error: true, 
+                    message: 'É necessário informar um nome para a nova Categoria'
+                });
+            };
+
+            if (!description) {
+                return res.status(401).json({
+                    error: true, 
+                    message: 'É necessário informar uma breve descrição para a nova Categoria'
+                });
+            };
+
+            const NewCategory = await CategoryModel.create({ name, description });
+
+            if (!NewCategory) {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Não foi possível cadastrar a nova categoria! Tente novamente mais tarde'
+                });
+            };
+
+            return res.status(201).json({
+                error: false,
+                category: NewCategory,
+                token: req.RefreshToken.JWT
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                error: true,
+                message: error.message
+            });
+            
+        };
+
+    }
 
 };
