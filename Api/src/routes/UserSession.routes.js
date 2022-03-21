b354f890c929adeb7efe3892cfe0b46593ba6d98
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 13:47:21 -0300

    Rota de Usuários

---
+++ Api/src/routes/UserSession.routes.js
@@ -0,0 +1,21 @@
+const routes = require('express').Router();
+const { CodeChecker } = require('../middlewares/Verify');
+
+const { 
+    CreateNewUser, 
+    ConnectUser,
+    ListOneUser,
+    ListUsers,
+    SendEmailToResetPassword,
+    RedefinePassword
+
+} = require('../controllers/UserSession');
+
+routes.post('/User/Create', CreateNewUser);
+routes.post('/User/Login', ConnectUser);
+routes.get('/User/:id', ListOneUser);
+routes.get('/User', ListUsers);
+routes.post('/User/SendEmail', SendEmailToResetPassword);
+routes.put('/User/RedefinePassword', CodeChecker, RedefinePassword);
+
+module.exports = routes;
