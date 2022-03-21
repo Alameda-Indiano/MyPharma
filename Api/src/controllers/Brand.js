8eed7343ccea75c110d8a7fc92bae135fd7c5cae
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 17:21:27 -0300

    Create Brand

--- Api/src/controllers/Brand.js
+++ Api/src/controllers/Brand.js
@@ -1,5 +1,40 @@
 const BrandModel = require('../models/Brand');
 
 module.exports = {
+    CreateNewBrand: async (req, res) => {
+        const { name } = req.body;
 
+        try {
+            if (!name) {
+                return res.status(401).json({
+                    error: true, 
+                    message: 'É necessário informar um nome para a nova Categoria'
+                });
+            };
+
+            const NewBrand = await BrandModel.create({ name });
+
+            if (!NewBrand) {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Não foi possível cadastrar a nova categoria! Tente novamente mais tarde'
+                });
+            };
+
+            return res.status(201).json({
+                error: false,
+                Brand: NewBrand,
+                token: req.RefreshToken.JWT
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                error: true,
+                message: error.message
+            });
+            
+        };
+
+    },
+    
 };
