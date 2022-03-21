5a72fb0c42943d9c19d92977ba9539a8da1e3bac
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-21 15:06:17 -0300

    Model de Marcas

---
+++ Api/src/models/Brand.js
@@ -0,0 +1,20 @@
+const mongoose = require('../DataBase/mongoDB');
+
+const BrandSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    }],
+    creatdAt: {
+        type: String,
+        default: Date.now
+    }
+});
+
+const BrandModel = mongoose.model('Brand', BrandSchema);
+module.exports = BrandModel;
