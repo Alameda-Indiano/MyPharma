f3e944d7f5ea9c37b58a9ec3ad2b80085058b496
Thiago Almeida Amorim <thiago2332003@gmail.com>
2022-03-29 16:42:02 -0300

    Rotas Permissions

---
+++ Api/src/routes/Permission.routes.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router();
+const { VerifyToken, RefreshToken } = require('../middlewares/Verify');
+
+const {
+    CreateNewPermission,
+    ListOnePermission,
+    ListPermission,
+    UpdatePermission,
+    DeletePermission
+    
+} = require('../controllers/Permission')
+
+routes.post('/Permission/Create', VerifyToken, RefreshToken, CreateNewPermission);
+routes.get('/Permission/:id', VerifyToken, RefreshToken, ListOnePermission);
+routes.get('/Permission', VerifyToken, RefreshToken, ListPermission);
+routes.put('/Permission/Atualize/:id', VerifyToken, RefreshToken, UpdatePermission);
+routes.delete('/Permission/Remove/:id', VerifyToken, RefreshToken, DeletePermission);
+
+module.exports = routes;
